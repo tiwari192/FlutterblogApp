ec6e0e0d50cbb184e9f9b8f1c59c36ee963dec25
Kagito <ktonline123456@gmail.com>
2021-10-04 23:13:03 +0530

    Hosted MongoDb on Atlas

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const password = require('./password')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 //Connection with MongoDb
-mongoose.connect('mongodb://localhost:27017/blog');
+const uri = `mongodb+srv://BlogDatabase:${password.password}@cluster0.e5fzw.mongodb.net/BlogDb?retryWrites=true&w=majority`;
+mongoose.connect(uri, {useNewUrlParser: true});
 
 //Connection Initiated
 const connection = mongoose.connection;
