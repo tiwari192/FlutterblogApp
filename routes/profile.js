d39159f3ab301ba63fc67bde648a7fdef9b84b29
Kagito <ktonline123456@gmail.com>
2021-10-09 19:28:30 +0530

    Edited rest api's

--- routes/profile.js
+++ routes/profile.js
@@ -16,19 +16,19 @@ const storage = multer.diskStorage({
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    if(file.mimetype == 'image/jpg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpeg'){
-        cb(null, true);
-    }
-    else{
-        cb(null, false);
-    }
-};
+// const fileFilter = (req, file, cb) => {
+//     if(file.mimetype == 'image/jpg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpeg'){
+//         cb(null, true);
+//     }
+//     else{
+//         cb(null, false);
+//     }
+// };
 
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1024 * 1024 * 5,
+        fileSize: 1024 * 1024 * 6,
     },
     
 });
